607979bb3aa579bb01e03cf1cc89d2c703e91723
hsnakd <hsnakd10@gmail.com>
2024-06-29 01:40:36 +0300

    Miaplaza Task Parents Info Added (Except Checkboxes) Students' Info Added (Except Checkboxes)

--- src/test/java/com/cydeo/pages/pages/Miaplaza_Page.java
+++ src/test/java/com/cydeo/pages/pages/Miaplaza_Page.java
@@ -12,44 +12,151 @@ public class Miaplaza_Page {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
+    // Online High School
     @FindBy(xpath = "//*[.='Online High School']")
     public WebElement onlineHighSchool;
 
-
+    // Apply to Our School
     @FindBy(xpath = "//*[.='Apply to Our School']")
     public WebElement applyToOurSchool;
 
 
-    @FindBy(xpath = "//*[.='Next']")
-    public WebElement nextButton;
 
+    // Student Information
 
     @FindBy(xpath = "//input[@name='Name' and @type='text']")
     public WebElement parent1FirstName;
-
-
-
-    @FindBy(xpath = "//input[@autocomplete='given-name' and @complink='Name2_First']")
-    public WebElement student1FirstName;
-
+    @FindBy(name = "Name1")
+    public WebElement parent2FirstName;
 
     @FindBy(xpath = "//input[@name='Name' and @elname='Last']")
     public WebElement parent1LastName;
-
+    @FindBy(xpath = "//input[@elname='Last' and @name='Name1']")
+    public WebElement parent2LastName;
 
     @FindBy(id = "Email-arialabel")
     public WebElement parent1Email;
-
-
-
-    @FindBy(id = "Email2-arialabel")
-    public WebElement student1Email;
-
+    @FindBy(id = "Email1-arialabel")
+    public WebElement parent2Email;
 
     @FindBy(id = "PhoneNumber")
     public WebElement parent1PhoneNumber;
+    @FindBy(id = "PhoneNumber1")
+    public WebElement parent2PhoneNumber;
 
+    @FindBy(xpath = "//*[@id='Dropdown-arialabel']")
+    public WebElement secondParentDropdown;
+
+    @FindBy(xpath = "//input[@id='Checkbox_1']")
+    public WebElement searchEngine;
+    @FindBy(xpath = "Checkbox_10")
+    public WebElement otherSocialMedia;
+
+    @FindBy(id = "Date-date")
+    public WebElement startDate;
+
+    @FindBy(xpath = "//span[@title='2024']")
+    public WebElement year;
+
+    @FindBy(xpath = "//input[@class='select2-search__field']")
+    public WebElement yearField;
+
+    @FindBy(xpath = "//*[.='Aug']")
+    public WebElement month;
+    @FindBy(xpath = "//*[.='26']")
+    public WebElement day;
+
+    @FindBy(xpath = "(//button[@type='button' and @elname='next'])[1]")
+    public WebElement nextButton;
+
+    // Student Information
+
+    @FindBy(id = "Dropdown1-arialabel")
+    public WebElement selectStudentNumber;
+
+    @FindBy(xpath = "//input[@autocomplete='given-name' and @complink='Name2_First']")
+    public WebElement student1FirstName;
+    @FindBy(xpath = "//input[@autocomplete='given-name' and @complink='Name3_First']")
+    public WebElement student2FirstName;
+
+    @FindBy(xpath = "//input[@name='Name2' and @elname='Last']")
+    public WebElement student1LastName;
+    @FindBy(xpath = "//input[@elname='Last' and @name='Name3']")
+    public WebElement student2LastName;
+
+    @FindBy(id = "Email2-arialabel")
+    public WebElement student1Email;
+    @FindBy(id = "Email3-arialabel")
+    public WebElement student2Email;
 
     @FindBy(id = "PhoneNumber2")
-    public WebElement parent2PhoneNumber;
+    public WebElement student1PhoneNumber;
+    @FindBy(id = "PhoneNumber3")
+    public WebElement student2PhoneNumber;
+
+    @FindBy(id = "SingleLine3-arialabel")
+    public WebElement student1DateOfBirth;
+    @FindBy(id = "SingleLine4-arialabel")
+    public WebElement student2DateOfBirth;
+
+    @FindBy(id = "Dropdown2-arialabel")
+    public WebElement student1Consent;
+    @FindBy(id = "Dropdown6-arialabel")
+    public WebElement student2Consent;
+
+    @FindBy(id = "Dropdown3-arialabel")
+    public WebElement student1Gender;
+    @FindBy(id = "Dropdown7-arialabel")
+    public WebElement student2Gender;
+
+    @FindBy(id = "Dropdown4-arialabel")
+    public WebElement student1Account;
+    @FindBy(id = "Dropdown8-arialabel")
+    public WebElement student2Account;
+
+    @FindBy(id = "Dropdown10-arialabel")
+    public WebElement student1Membership;
+    @FindBy(id = "Dropdown8-arialabel")
+    public WebElement student2Membership;
+
+    @FindBy(id = "Dropdown5-arialabel")
+    public WebElement student1Schooling;
+    @FindBy(id = "Dropdown9-arialabel")
+    public WebElement student2Schooling;
+
+    @FindBy(xpath = "//*[@id='Checkbox1_2']")
+    public WebElement student1Math1;
+    @FindBy(id = "Checkbox1_5")
+    public WebElement student1Math2;
+
+    @FindBy(id = "Checkbox2_1")
+    public WebElement student1English1;
+    @FindBy(id = "Checkbox2_2")
+    public WebElement student1English2;
+
+    @FindBy(id = "Checkbox3_1")
+    public WebElement student1Science1;
+    @FindBy(id = "Checkbox3_4")
+    public WebElement student1Science2;
+
+
+    @FindBy(id = "MultiLine-arialabel")
+    public WebElement student1List;
+    @FindBy(id = "MultiLine2-arialabel")
+    public WebElement student2List;
+
+    @FindBy(id = "Dropdown13-arialabel")
+    public WebElement student1Needs;
+    @FindBy(id = "Dropdown14-arialabel")
+    public WebElement student2Needs;
+
+    @FindBy(id = "MultiLine1-arialabel")
+    public WebElement student1Describe;
+
+
+
+
+
+
+
 }
--- src/test/java/com/cydeo/step_definitions/Miaplaza_SD.java
+++ src/test/java/com/cydeo/step_definitions/Miaplaza_SD.java
@@ -4,21 +4,29 @@ import com.cydeo.pages.CrmProjectTask_Page;
 import com.cydeo.pages.pages.Miaplaza_Page;
 import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.Driver;
+import com.github.javafaker.Faker;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+import java.text.SimpleDateFormat;
+
+import static org.junit.Assert.assertTrue;
 
 public class Miaplaza_SD {
 
     Miaplaza_Page miaplazaPage = new Miaplaza_Page();
-
+    Faker faker = new Faker();
 
     @Given("The user is on the MiAcademy website")
     public void theUserIsOnTheMiAcademyWebsite() {
-        Driver.getDriver().get("https://miacademy.co/#/");
+//        Driver.getDriver().get("https://miacademy.co/#/");
+        Driver.getDriver().get("https://forms.zohopublic.com/miaplazahelp/form/MOHSInitialApplication/formperma/okCyt4yyq39rZvSBXB9FSjDeek1ilbRVK1iNCK--3K8");
     }
 
     @When("The user navigates to the MiaPrep Online High School page")
@@ -33,15 +41,140 @@ public class Miaplaza_SD {
 
     @And("The user fills in the Parent Information with {string} and {string}")
     public void theUserFillsInTheParentInformationWithAnd(String arg0, String arg1) {
-        miaplazaPage.parent1FirstName.sendKeys("arg0");
-        miaplazaPage.parent1LastName.sendKeys("arg1");
-        miaplazaPage.parent1Email.sendKeys("hakdogan@live.com");
-        miaplazaPage.parent1PhoneNumber.sendKeys("+905469562626");
-        BrowserUtils.sleep(3);
+//        Select selectState = new Select(driver.findElement(By.xpath("//select[@id='state']")));
+//        selectState.selectByValue("CA");
+//        selectState.selectByVisibleText("California");
+//        selectState.selectByIndex(5);
+
+        // Parent Information
+        miaplazaPage.parent1FirstName.sendKeys(faker.name().firstName());
+        miaplazaPage.parent1LastName.sendKeys(faker.name().lastName());
+        miaplazaPage.parent1Email.sendKeys(faker.internet().emailAddress());
+        miaplazaPage.parent1PhoneNumber.sendKeys(faker.phoneNumber().cellPhone());
+
+        Select secondParentDropdown = new Select(miaplazaPage.secondParentDropdown);
+        secondParentDropdown.selectByVisibleText("Yes");
+
+        // Second Parent Information
+        miaplazaPage.parent2FirstName.sendKeys(faker.name().firstName());
+        miaplazaPage.parent2LastName.sendKeys(faker.name().lastName());
+        miaplazaPage.parent2Email.sendKeys(faker.internet().emailAddress());
+        miaplazaPage.parent2PhoneNumber.sendKeys(faker.phoneNumber().cellPhone());
+
+        // How did you hear about us? (Select all that apply)
+//        System.out.println("miaplazaPage.searchEngine.getText() = " + miaplazaPage.searchEngine.getText());
+//        miaplazaPage.searchEngine.click();
+//        assertTrue(miaplazaPage.searchEngine.isSelected());
+//        miaplazaPage.otherSocialMedia.click();
+//        assertTrue(miaplazaPage.otherSocialMedia.isSelected());
+
+        // What is your preferred start date? *
+        miaplazaPage.startDate.click();
+        miaplazaPage.year.click();
+        miaplazaPage.yearField.sendKeys("2024");
+
+        miaplazaPage.month.click();
+
+        miaplazaPage.day.click();
+
+        // Click the Next Button
+        miaplazaPage.nextButton.click();
+
+        BrowserUtils.sleep(5);
+
+
     }
 
     @And("The user proceeds to the Student Information page")
     public void theUserProceedsToTheStudentInformationPage() {
+
+        BrowserUtils.sleep(2);
+        Select secondParentDropdown = new Select(miaplazaPage.selectStudentNumber);
+        secondParentDropdown.selectByVisibleText("Two");
+
+        /** Student Information */
+        miaplazaPage.student1FirstName.sendKeys(faker.name().firstName());
+        miaplazaPage.student1LastName.sendKeys(faker.name().lastName());
+        miaplazaPage.student1Email.sendKeys(faker.internet().emailAddress());
+        miaplazaPage.student1PhoneNumber.sendKeys(faker.phoneNumber().cellPhone());
+
+        Select student1Consent = new Select(miaplazaPage.student1Consent);
+        student1Consent.selectByVisibleText("Yes");
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        String student1DateOfBirth = sdf.format(faker.date().birthday(10,18));
+        miaplazaPage.student1DateOfBirth.sendKeys(student1DateOfBirth);
+
+        Select student1Gender = new Select(miaplazaPage.student1Gender);
+        student1Gender.selectByVisibleText("Male");
+
+        Select student1Account = new Select(miaplazaPage.student1Account);
+        student1Account.selectByVisibleText("Yes");
+
+        Select student1Membership = new Select(miaplazaPage.student1Membership);
+        student1Membership.selectByVisibleText("Monthly");
+
+        Select student1Schooling = new Select(miaplazaPage.student1Schooling);
+        student1Schooling.selectByVisibleText("Private School");
+
+        // For each subject area, check off courses this student has already completed and earned credit for.
+//        miaplazaPage.student1Math1.click();
+//        miaplazaPage.student1Math2.click();
+//        miaplazaPage.student1English1.click();
+//        miaplazaPage.student1English2.click();
+//        miaplazaPage.student1Science1.click();
+//        miaplazaPage.student1Science2.click();
+
+        miaplazaPage.student1List.sendKeys(faker.lorem().paragraph());
+
+        Select student1Needs = new Select(miaplazaPage.student1Needs);
+        student1Needs.selectByVisibleText("Yes");
+
+        miaplazaPage.student1Describe.sendKeys(faker.lorem().paragraph());
+
+
+
+        /** Student Two */
+        miaplazaPage.student2FirstName.sendKeys(faker.name().firstName());
+        miaplazaPage.student2LastName.sendKeys(faker.name().lastName());
+        miaplazaPage.student2Email.sendKeys(faker.internet().emailAddress());
+        miaplazaPage.student2PhoneNumber.sendKeys(faker.phoneNumber().cellPhone());
+
+        Select student2Consent = new Select(miaplazaPage.student2Consent);
+        student2Consent.selectByVisibleText("No");
+
+        String student2DateOfBirth = sdf.format(faker.date().birthday(10,18));
+        miaplazaPage.student2DateOfBirth.sendKeys(student2DateOfBirth);
+
+        Select student2Gender = new Select(miaplazaPage.student2Gender);
+        student2Gender.selectByVisibleText("Female");
+
+        Select student2Account = new Select(miaplazaPage.student2Account);
+        student2Account.selectByVisibleText("No");
+
+//        Select student2Membership = new Select(miaplazaPage.student2Membership);
+//        student2Membership.selectByVisibleText("Monthly");
+
+        Select student2Schooling = new Select(miaplazaPage.student2Schooling);
+        student2Schooling.selectByVisibleText("Homeschool");
+
+        // For each subject area, check off courses this student has already completed and earned credit for.
+//        miaplazaPage.student1Math1.click();
+//        miaplazaPage.student1Math2.click();
+//        miaplazaPage.student1English1.click();
+//        miaplazaPage.student1English2.click();
+//        miaplazaPage.student1Science1.click();
+//        miaplazaPage.student1Science2.click();
+
+        miaplazaPage.student2List.sendKeys(faker.lorem().paragraph());
+
+        Select student2Needs = new Select(miaplazaPage.student2Needs);
+        student2Needs.selectByVisibleText("No");
+
+//        miaplazaPage.student2Describe.sendKeys(faker.lorem().paragraph());
+
+
+        BrowserUtils.sleep(15);
     }
 
     @And("The user fills in the Student Information with {string} and {string}")
